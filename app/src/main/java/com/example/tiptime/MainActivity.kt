1b99bd90c607b0867e455c779502d0c2e712af6f
Scott Young <scott.young@getbridge.com>
2023-01-06 13:57:07 -0700

    add calculator logic

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.widget.Switch
 import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +15,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener{ calculateTip() }
     }
 
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
 
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_five_percent -> 0.25
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_fifteen_percent -> 0.15
+            else -> 0.10
+        }
+
+        var tip = tipPercentage * cost
+
+        val roundUp = binding.roundUpSwitch.isChecked
+
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
 }
+
